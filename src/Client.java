dcd843d4b8974c2fb0b7502277d9fa6922e31ded
vmadmin <vmadmin@vmLP1.(none)>
2013-04-23 16:14:59 +0200

    Committer: philippsauter  On branch master 	modified:   src/Client.java 	new file:   src/menu/Menu.java

--- src/Client.java
+++ src/Client.java
@@ -1,12 +1,16 @@
 import observer.*;
+import menu.*;
 import graphics.*;
 import factory.*;
 
 
 public class Client {
 	public static void main(String[] args) {
+		Menu Options = new Menu();
+	}
+	public static void initiate(String level){
 		DeltaUpdater deltaUpdater = new DeltaUpdater();
-		new Spawner("level1", deltaUpdater);
+		new Spawner(level, deltaUpdater);
 		//Spawner LevelSpawner = new Spawner("level1", deltaUpdater);
 		GS graphicSync = new GS(deltaUpdater);
 		graphicSync.start();
---
+++ src/menu/Menu.java
@@ -0,0 +1,12 @@
+package menu;
+
+import javax.swing.JFrame;
+
+public class Menu {
+	protected JFrame window;
+	public Menu(){
+		window = new JFrame();
+		window.setSize(200, 200);
+		window.setVisible(true);
+	}
+}
