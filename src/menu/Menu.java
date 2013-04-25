b2755633de0474ce4414e1455e9f3de403cdab2b
philippsauter <philipp.sauter123@gmail.com>
2013-04-25 20:35:35 +0200

    On branch master 	new file:   json/menu/menu.json 	new file:   src/menu/KeyConfiguration.java 	modified:   src/menu/Menu.java

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -5,101 +5,92 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Vector;
 
 import javax.swing.*;
 
 import org.lwjgl.input.Keyboard;
 
-public class Menu extends JFrame implements KeyListener{
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class Menu extends JFrame{
 	public static JPanel jp = new JPanel();
 	public static JLabel jl;
+	Vector<JButton> Buttons = new Vector<JButton>();
+	
+	private class TextSwitcher implements KeyListener{
+		public JButton Owner;
+		public TextSwitcher(JButton Owner){
+			this.Owner = Owner;
+		}
+		public void keyPressed(KeyEvent arg0) {}
+		public void keyReleased(KeyEvent arg0) {}
+		public void keyTyped(KeyEvent arg0) {
+			Owner.setText(new String(new char[]{arg0.get.getKeyChar()}));
+		}
+	}
 	public Menu(){
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(200, 200);
 		this.setBounds(0,0,200,200);
-
-		JButton Start = new JButton("Start");
-		JButton firekey = new JButton("firekey");
-		JButton chargekey = new JButton("chargekey");
-		JButton keepFiring = new JButton("keepFiring");
-		JButton moveUp = new JButton("moveUp");
-		JButton moveDown = new JButton("moveDown");
-		JButton moveLeft = new JButton("moveLeft");
-		JButton moveRight = new JButton("moveRight");
 		
-		Start.setSize(100, 10);
-		firekey.setSize(100, 10);
-		chargekey.setSize(100, 10);
-		keepFiring.setSize(100, 10);
-		moveUp.setSize(100, 10);
-		moveDown.setSize(100, 10);
-		moveLeft.setSize(100, 10);
-		moveRight.setSize(100, 10);
+		String[] Names = new String[]{
+				"Start",
+				"firekey",
+				"chargekey",
+				"keepFiring",
+				"moveUp",
+				"moveDown",
+				"moveLeft",
+				"moveRight"
+		};
+		Vector<JLabel> Labels = new Vector<JLabel>();
+		for(int i=0;i<Names.length;++i){
+			Labels.add(new JLabel(Names[i]));
+			jp.add(Labels.lastElement());
+			Buttons.add(new JButton(Names[i]));
+			Buttons.lastElement().setSize(100, 10);
+			Buttons.lastElement().addKeyListener(new TextSwitcher(Buttons.lastElement()));
+			jp.add(Buttons.lastElement());
+		}
 		
-		JLabel labelstart = new JLabel("Start");
-		jp.add(labelstart);
-		jp.add(Start, BorderLayout.CENTER);
-		Start.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e)
-            {
-            	Menu.jl = (JLabel)jp.getComponent(0);
-            }
+		JButton Speichern = new JButton("Speichern");
+		Speichern.addMouseListener(new MouseListener(){
+			public void mouseClicked(MouseEvent arg0) {
+				Vector<String> Keys = new Vector<String>();
+				for(JButton Button: Buttons){
+					Keys.add(Button.getText());
+				}
+				try {
+					String w = (new Gson()).toJson(Keys, (new TypeToken<Vector<String>>(){}).getType());
+					BufferedWriter bw = new BufferedWriter(new FileWriter("json/menu/menu.json"));
+					bw.write(w);
+					bw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			public void mouseEntered(MouseEvent arg0) {
+			}
+			public void mouseExited(MouseEvent arg0) {
+			}
+			public void mousePressed(MouseEvent arg0) {
+			}
+			public void mouseReleased(MouseEvent arg0) {
+			}
 		});
-		jp.add(new JLabel("firekey"));
-		jp.add(firekey, BorderLayout.LINE_END);
-		jp.add(new JLabel("chargekey"));
-		jp.add(chargekey);
-		jp.add(new JLabel("keepFiring"));
-		jp.add(keepFiring);
-		jp.add(new JLabel("moveUp"));
-		jp.add(moveUp);
-		jp.add(new JLabel("moveDown"));
-		jp.add(moveDown);
-		jp.add(new JLabel("moveLeft"));
-		jp.add(moveLeft);
-		jp.add(new JLabel("moveRight"));
-		jp.add(moveRight);
+		jp.add(Speichern);
 
-		
-		
 		this.getContentPane().add(jp);
 		this.pack();
 		this.setVisible(true);
-		/*
-		this.add(new JButton("Start");
-		this.add(new JButton("firekey"));
-		this.add(new JButton("chargekey"));
-		this.add(new JButton("keepFiring"));
-		this.add(new JButton("moveUp"));
-		this.add(new JButton("moveDown"));
-		this.add(new JButton("moveLeft"));
-		this.add(new JButton("moveRight"));
-		*/
-		/*
-		firekey = Keyboard.KEY_A;
-		chargekey = Keyboard.KEY_S;
-		keepFiring = Keyboard.KEY_D;
-		moveUp = Keyboard.KEY_UP;
-		moveDown = Keyboard.KEY_DOWN;
-		moveLeft = Keyboard.KEY_LEFT;
-		moveRight = Keyboard.KEY_RIGHT;
-		*/
 	}
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-	}
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		Menu.jl.setText(new String(new char[]{e.getKeyChar()}));
-		System.out.println("hello");
-		
-	}
+
 }
